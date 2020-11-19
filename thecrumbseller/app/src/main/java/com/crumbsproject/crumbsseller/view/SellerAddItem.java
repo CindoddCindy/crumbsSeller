1872b804f5a2c5e911324cf6fbe3086e6e98bc19
CindoddCindy <cindodcindy@gmail.com>
2020-11-19 14:46:14 +0700

    tambah, dan pindah hlaman

--- thecrumbseller/app/src/main/java/com/crumbsproject/crumbsseller/view/SellerAddItem.java
+++ thecrumbseller/app/src/main/java/com/crumbsproject/crumbsseller/view/SellerAddItem.java
@@ -2,15 +2,33 @@ package com.crumbsproject.crumbsseller.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.crumbsproject.crumbsseller.R;
 
 public class SellerAddItem extends AppCompatActivity {
 
+    private Button button_add_my_item;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seller_add_item);
+
+        button_add_my_item=findViewById(R.id.btm_add_items);
+        button_add_my_item.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SellerAddItem.this,BottomNavigationSeller.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
     }
 }
