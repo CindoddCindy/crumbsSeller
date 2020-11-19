fd196ef99472a6bcf73f1429fe14481743aea7fe
CindoddCindy <cindodcindy@gmail.com>
2020-11-19 15:27:55 +0700

    contoh alur

--- thecrumbseller/app/src/main/java/com/crumbsproject/crumbsseller/view/fragment/SellerProfile.java
+++ thecrumbseller/app/src/main/java/com/crumbsproject/crumbsseller/view/fragment/SellerProfile.java
@@ -1,5 +1,6 @@
 package com.crumbsproject.crumbsseller.view.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,8 +8,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.crumbsproject.crumbsseller.R;
+import com.crumbsproject.crumbsseller.view.RegisSeller;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,6 +28,8 @@ public class SellerProfile extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private Button button_logout_seller;
 
     public SellerProfile() {
         // Required empty public constructor
@@ -61,6 +66,17 @@ public class SellerProfile extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_seller_profile, container, false);
+         View view= inflater.inflate(R.layout.fragment_seller_profile, container, false);
+
+         button_logout_seller=view.findViewById(R.id.btn_logout_seller);
+         button_logout_seller.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 Intent intent = new Intent(getActivity(), RegisSeller.class);
+                 startActivity(intent);
+             }
+         });
+
+        return view;
     }
 }
